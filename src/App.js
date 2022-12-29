4b76cf104b91071c12fc3bf00cee09098da11b93
xusui <xusui@cisco.com>
2022-12-29 19:55:18 +0800

    2022-12-29

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import HomePage from './HomePage/HomePage';
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      {/* <header className="App-header">
+        
+      </header> */}
+      <HomePage></HomePage>
+      </ThemeProvider>
     </div>
   );
 }
